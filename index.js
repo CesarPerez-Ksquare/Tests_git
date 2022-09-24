55d2f71e8bdbd228af3b5c3d7fbb1688382b07ba
Cesar Perez <cesar.perez@itksquare.edu.mx>
2022-09-24 14:59:06 -0400

    Added US7

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+// Cesar US
+ 
+// User Story 7.
+  const element = document.getElementById("restartButton");
+  element.addEventListener("click", myFunction);
+  element.addEventListener("click", us8);
+  let pattern = 0;
+
+  function myFunction() {
+      document.getElementById("restarted").innerHTML = "Game have been restarted. ";
+      pattern = 0;
+  };
+
+  // User Story 8.
+  function us8() {
+      pattern = 20;
+      if (pattern === 20) {
+      alert("Winner");
+  }
+  }
+  
