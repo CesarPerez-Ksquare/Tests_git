f74441f3dfac46ded4a4d70aa0ca5d1b99022a43
Cesar Perez <cesar.perez@itksquare.edu.mx>
2022-09-25 12:41:57 -0400

    User Story 8

--- index.js
+++ index.js
@@ -12,4 +12,12 @@
       document.getElementById("restarted").innerHTML = "Game have been restarted. ";
       pattern = 0;
   };
+
+  // User Story 8.
+  function us8() {
+    pattern = 20;
+    if (pattern === 20) {
+    confirm("Congratulations!! You are the winner! \n Do you want to start a new game? ");
+}
+}
   
